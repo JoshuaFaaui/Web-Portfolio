3cdeb7692f183c6bb6a8b10d623d181a0396b261
JoshuaFaaui <joshuafaaui@gmail.com>
2023-05-03 20:23:11 +1200

    nother attempt

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import Home from './pages/Home';
 
 function App() {
   return (
+    <>
+      <h1>LEO</h1>
     <Routes>
       <Route path='/' element={<Home/>} />
     </Routes>
+    </>
+    
   );
 }
 
