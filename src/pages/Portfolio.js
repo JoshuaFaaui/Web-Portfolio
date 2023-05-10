1b3b9b9e285208d8efacd331cf92109a6d2c6256
JoshuaFaaui <joshuafaaui@gmail.com>
2023-05-10 16:06:24 +1200

    some updates to portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Animator,batch, Fade, FadeOut, MoveIn, MoveOut,ScrollContainer, ScrollPage,ZoomOut } from 'react-scroll-motion';
 import '../styles/Portfolio.sass'
 import ForgotPasswordVid from '../Assets/ForgotPassword.mp4'
@@ -7,23 +7,26 @@ import { useRef } from 'react';
 import CustomText from '../components/CustomText';
 import StripeSubscription from '../Assets/StripeSubscription.mp4'
 function Portfolio() {
-    const vid1 = useRef(null);
-    const isinview1 = useInView(vid1, { once: true });
+    
+  const vid1 = useRef(null);
+  const isinview1 = useInView(vid1);
+  const vid2 = useRef(null);
+  const isinview2 = useInView(vid2);
   return (
     <div>
-      <CustomText text="Portfolio"/>
+      <CustomText text="Services"/>
       <motion.div initial={{ x: "-100vw" }} animate={{ x: 0 }} transition={{ delay:.3 }} className='VideosContainer'>
-        <h1>i've managed users accounts</h1>
+        <h1>I offer user account systems into websites</h1>
         <h2>(Forgot Password Process)</h2>
         <video src={ForgotPasswordVid} muted={true} autoPlay={true} controls />
-        <p>I offer account managing(login&signup) processes in your website</p>
+        <p>Here am I as a user, going through a reset password process<br/>which i've built</p>
       </motion.div>
       <section ref={vid1}>
         <motion.div initial={{ x: "-100vw" }} animate={isinview1 ? { x: 0 } : undefined} transition={{ delay: .5 }} className='VideosContainer'>
-          <h1>i've managed stripe<br/>subscriptions & payments in websites</h1>
+          <h1>I offer stripe<br/>subscriptions & payments into websites</h1>
           <h2>(Stripe subscription process)</h2>
           <video src={StripeSubscription} muted={true} autoPlay={true} controls />
-          <p>Payment or subscription services to your website is available</p>
+          <p>Here I am going through a signup and subscription process for a website i've built</p>
         </motion.div>
       </section>
     </div>
