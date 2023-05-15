0401fbb5bbda7299e68384e0602ef894c21bf602
JoshuaFaaui <joshuafaaui@gmail.com>
2023-05-15 19:44:49 +1200

    final push

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeNav from '../components/Home/HomeNav'
 import { motion } from 'framer-motion';
 import CustomText from '../components/CustomText';
 import '../styles/Home/Content/Home.sass'
 import InstaLogo from '../Assets/InstaLogo.png'
 import TwitterLogo from '../Assets/TwitterLogo.png'
+import axios from 'axios';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 function Home() {
+  const [usersEmail, setUsersEmail] = useState('')
+  const [usersMessage,setUsersMessage] = useState('')
+  const SendMail = (e) => {
+    e.preventDefault()
+    axios.post('https://web-served.onrender.com', {
+      users_email: usersEmail,
+      users_message:usersMessage
+    })
+    setUsersEmail('')
+    setUsersMessage('')
+    alert('email successfully sent, will get back to you soon. Have a great day!')
+  }
   return (
     <motion.div className="w-screen h-screen" id="HomeFoundation" initial={{opacity:-1}} animate={{opacity:1,transition:{duration:1}}} exit={{opacity:-1,transition:{duration:1}}}>
       <HomeNav/>
@@ -25,6 +40,12 @@ function Home() {
           </ul>
         </div>
       </div>
+      <form id='emailform' onSubmit={SendMail}>
+        <h1>Contact Me</h1>
+        <TextField required variant='outlined' label="your email" className='emailinputs' autoComplete='off' value={usersEmail} onChange={(e)=>setUsersEmail(e.target.value)}/>
+        <TextField required variant='outlined' label="message" className='emailinputs' autoComplete='off' value={usersMessage} onChange={(e)=>setUsersMessage(e.target.value)}/>
+        <Button type="submit" variant="contained" id='emailsubmit'>Send</Button>
+      </form>
     </motion.div>
   );
 }
