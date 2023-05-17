2d482501c3828127a5cca9e93a95f06b59a7a298
JoshuaFaaui <joshuafaaui@gmail.com>
2023-05-18 02:35:29 +1200

    changes to custom doomain

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,11 +25,12 @@ function Home() {
     <motion.div className="w-screen h-screen" id="HomeFoundation" initial={{opacity:-1}} animate={{opacity:1,transition:{duration:1}}} exit={{opacity:-1,transition:{duration:1}}}>
       <HomeNav/>
       <CustomText text="Web Served" />
-      <h2>Welcome to Web Served! we serve current & potential customers<br />such as small or private businesses, eccomerce businesses, entrepreurs & anyone who may be looking to have an online store built and hosted</h2>
+      <h1 id='WelcomeHomeHeader'>Welcome to Web Served!</h1>
+      <h2>We serve current & potential customers such as<br />small or private businesses, eccomerce businesses, entrepreurs & anyone who may be looking to have an online store built and hosted.</h2>
       <div id='aboutmecontainer'>
         <div id='aboutmetext'>
           <h1>About Me</h1>
-          <p>I am a intelligent young 17 yr old software developer, with an entrepreneurial mindset to help others with their business, along our journey to success</p>
+          <p>I am a young 17 yr old software developer, with an entrepreneurial mindset to help others with their business, along our journey to success!</p>
         </div>
         <div id='socialmedia'>
           <h1>Social Media</h1>
